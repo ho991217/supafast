bd9d63e928f4084ce6218e218fc193179b37ea5d
이호연 <ho991217@kakao.com>
2024-02-19 22:51:40 +0900

    feat: 라우트 분리

--- src/components/home/live-channels/live-channels-skeleton.tsx
+++ src/components/home/live-channels/live-channels-skeleton.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { LiveCardSkeleton } from '@components/common';
-import { Category } from '@/assets/types/common';
-import Header from './header';
 
-export default async function Skeleton({
-  currentPath,
-}: {
-  currentPath: Lowercase<Category>;
-}) {
+export default async function Skeleton() {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="grid grid-cols-5 place-items-start gap-x-[13px] gap-y-[19px]">
